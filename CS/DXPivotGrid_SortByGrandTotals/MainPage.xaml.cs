2cefce1baa78d40d44f94d1e469d30cb1fbb8005
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_SortByGrandTotals/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+using System.Reflection;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DXPivotGrid_SortByGrandTotals {
+    public partial class MainPage : UserControl {
+        string dataFileName = "DXPivotGrid_SortByGrandTotals.nwind.xml";
+        public MainPage() {
+            InitializeComponent();
+
+            // Parses an XML file and creates a collection of data items.
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream stream = assembly.GetManifestResourceStream(dataFileName);
+            XmlSerializer s = new XmlSerializer(typeof(OrderData));
+            object dataSource = s.Deserialize(stream);
+
+            // Binds a pivot grid to this collection.
+            pivotGridControl1.DataSource = dataSource;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to: Sort Field Values by the Grand Total Column (Row)
+
+
+<p>This example shows how to sort data by Grand Total values.</p><p>In this example, values of the Country field are sorted by the Grand Total column. To do this, the data field by whose values the sorting should be performed (the Freight field) is assigned to the Country field's PivotGridField.SortByField property. </p><br />
+
+
+<br/>
+
+
